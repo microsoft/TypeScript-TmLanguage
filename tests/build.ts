de051b5d7b504c5abdbe07a73bca67da4dc6bb64
zhengbli <zhengbli@microsoft.com>
2015-11-19 13:47:51 -0800

    Add mocha as test runner

---
+++ tests/build.ts
@@ -0,0 +1,55 @@
+/// <reference path="typings/node/node.d.ts" />
+
+import * as vt from 'vscode-textmate/release/main';
+import fs = require('fs');
+
+var register = new vt.Registry();
+var tsGrammar = register.loadGrammarFromPathSync("../TypeScript.tmLanguage");
+var tsReactGrammar = register.loadGrammarFromPathSync("../TypeScriptReact.tmLanguage");
+
+const marker = '^';
+
+function deleteCharAt(index: number, str: string) {
+    return str.slice(0, index) + str.slice(index + 1);
+}
+
+function getMarkerLocations(str: string):  number[] {
+    let locations: number[] = [];
+    let markerLocation = str.indexOf(marker);
+    while (markerLocation >= 0) {
+        locations.push(markerLocation);
+        str = deleteCharAt(markerLocation, str);
+        markerLocation = str.indexOf(marker);
+    }
+    return locations;
+}
+
+function getScopesAtMarkers(text: string, grammar: vt.IGrammar): string {
+    let oriLines = text.split('\n');
+    let ruleStack = undefined;
+    let outputLines: string[] = [];
+    for (let i in oriLines) {
+        let oriLine = oriLines[i];
+        let markerLocations = getMarkerLocations(oriLine);
+        let line = oriLine.split(marker).join('');
+        let lineTokens = grammar.tokenizeLine(line, ruleStack);
+        ruleStack = lineTokens.ruleStack;
+
+        for (let token of lineTokens.tokens) {
+            for (let markerLocation of markerLocations) {
+                if (token.startIndex <= markerLocation && markerLocation < token.endIndex) {
+                    outputLines.push('[' + (parseInt(i) + 1) + ', ' + (markerLocation + 1) + ']: ' + token.scopes.join(' ') + ' ');
+                }
+            }
+        }
+    }
+
+    return outputLines.join('\n');
+}
+
+for (var file of fs.readdirSync('cases')) {
+    var text = fs.readFileSync('cases/' + file, 'utf8');
+    let outputFilename = file.slice(0, file.lastIndexOf('.'));
+    let grammer = file.slice(file.indexOf('.tsx')) === '.tsx' ? tsReactGrammar : tsGrammar;
+    fs.writeFile('generated/' + outputFilename + '.txt', getScopesAtMarkers(text, grammer), "utf8");
+}
--- tests/test.ts
+++ tests/test.ts
@@ -1,55 +1,23 @@
-/// <reference path="typings/node/node.d.ts" />
+/// <reference path="typings/mocha/mocha.d.ts" />
+/// <reference path="typings/chai/chai.d.ts" />
 
-import * as vt from 'vscode-textmate/release/main';
 import fs = require('fs');
+import chai = require('chai');
 
-var register = new vt.Registry();
-var tsGrammar = register.loadGrammarFromPathSync("../TypeScript.tmLanguage");
-var tsReactGrammar = register.loadGrammarFromPathSync("../TypeScriptReact.tmLanguage");
-
-const marker = '^';
-
-function deleteCharAt(index: number, str: string) {
-    return str.slice(0, index) + str.slice(index + 1);
+function compareText(text1: string, text2: string, ignoreWhiteSpace = true, ignoreNewLine = true){
+	text1 = text1.split('\r\n').join('').split('\n').join('');
+	text2 = text2.split('\r\n').join('').split('\n').join('');
+	return text1 === text2;
 }
 
-function getMarkerLocations(str: string):  number[] {
-    let locations: number[] = [];
-    let markerLocation = str.indexOf(marker);
-    while (markerLocation >= 0) {
-        locations.push(markerLocation);
-        str = deleteCharAt(markerLocation, str);
-        markerLocation = str.indexOf(marker);
-    }
-    return locations;
-}
-
-function getScopesAtMarkers(text: string, grammar: vt.IGrammar): string {
-    let oriLines = text.split('\n');
-    let ruleStack = undefined;
-    let outputLines: string[] = [];
-    for (let i in oriLines) {
-        let oriLine = oriLines[i];
-        let markerLocations = getMarkerLocations(oriLine);
-        let line = oriLine.split(marker).join('');
-        let lineTokens = grammar.tokenizeLine(line, ruleStack);
-        ruleStack = lineTokens.ruleStack;
-
-        for (let token of lineTokens.tokens) {
-            for (let markerLocation of markerLocations) {
-                if (token.startIndex <= markerLocation && markerLocation < token.endIndex) {
-                    outputLines.push('[' + (parseInt(i) + 1) + ', ' + (markerLocation + 1) + ']: ' + token.scopes.join(' ') + ' ');
-                }
-            }
-        }
-    }
-
-    return outputLines.join('\n');
-}
-
-for (var file of fs.readdirSync('cases')) {
-    var text = fs.readFileSync('cases/' + file, 'utf8');
-    let outputFilename = file.slice(0, file.lastIndexOf('.'));
-    let grammer = file.slice(file.indexOf('.tsx')) === '.tsx' ? tsReactGrammar : tsGrammar;
-    fs.writeFile('generated/' + outputFilename + '.txt', getScopesAtMarkers(text, grammer), "utf8");
-}
+describe("Compare generated with baselines", () => {
+	for (var file of fs.readdirSync('generated')) {
+		var generatedText = fs.readFileSync('generated/' + file, 'utf8');
+		var baselinesText = fs.readFileSync('baselines/' + file, 'utf8');
+		describe('Comparing ' + file, () => {
+			it('should be the same', () => {
+				chai.expect(compareText(generatedText, baselinesText)).to.true;
+			})
+		})
+	}
+})
