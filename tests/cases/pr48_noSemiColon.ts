dad3eead1743a5492604d125ddc8b1a2dd976662
zhengbli <zhengbli@microsoft.com>
2016-01-27 01:58:22 -0800

    Add test case for PR48

---
+++ tests/cases/pr48_noSemiColon.ts
@@ -0,0 +1,20 @@
+class Test {
+	^private ^data: TemplateData[] = []
+	^private ^options: TemplateOptions = {}
+	^private ^state = {}
+}
+
+^export ^type ^Stats = fs.Stats
+^export ^const ^touch = thenify(tch)
+
+/**
+ * Verify comments
+ */
+^export ^function isFile(path: string): ^Promise<boolean> {
+	^return stat(path).then(stat => stat.isFile(), () => false)
+}
+
+
+^export ^const readFile = thenify<^string, string, string>(fs.readFile)
+^export ^const writeFile = thenify<^string, string | Buffer, void>(fs.writeFile)
+
