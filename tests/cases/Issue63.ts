fabf9fe01ae4c2572445f93085d06cdf8c7e47e3
Anubha Mathur <anubmat@microsoft.com>
2016-02-25 12:38:13 -0800

    Updated the tests to include Issue63.ts

--- tests/cases/Issue63.ts
+++ tests/cases/Issue63.ts
@@ -1,7 +1,7 @@
-^type ^Callback <^string> = ^(error: Error, value: T) => any
+^^type ^^Callback <^^string> = ^^(error: Error, value: T) => any
 
-^type Callback^<string > = ^(error: Error, value: T) ^=> ^any
+^^type Callback^^<string > = ^^(error: Error, value: T) ^^=> ^^any
 
-type Callback = ^(error: Error, value: T) => ^any
+type Callback = ^^(error: Error, value: T) => ^^any
 
-^type ^Callback^<string ^| ^(a: number, b: string) => ^void> = ^(error: Error, value: T) => ^any
+^^type ^^Callback^^<string ^^| ^^(a: number, b: string) => ^^void> = ^^(error: Error, value: T) => ^^any
