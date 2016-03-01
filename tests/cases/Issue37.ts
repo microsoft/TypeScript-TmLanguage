e21186a73a453b970cb4bb3cd186d62e8696fef1
Anubha Mathur <anubmat@microsoft.com>
2016-03-01 15:20:23 -0800

    Solving Issue 37, changing scopes of major keywords, changing scope of switch statement, modified type-annotation to allow typeof keyword

---
+++ tests/cases/Issue37.ts
@@ -0,0 +1,48 @@
+//Test For Scopes
+
+^^import ^^* ^^as req ^^from "./Required"
+import { Scale } from "./scale";
+
+^^if (object.^^instanceof(ClassNew)) {
+}
+
+^^class ^^class1(){
+	^^static ^^functioname(){
+
+	}
+}
+
+^^export ^^class className(){
+	^^private ^^state = { }
+}
+
+^^void ^^newFunctionName(){
+
+	^^var c = a ^^+ b;
+
+	^^switch (someNum) {
+		^^case 1: break;
+		^^case 2: break;
+		^^default: break;
+	} 
+
+}
+
+/**
+ * Verify comments
+ */
+
+^^module M {
+    ^^export function myFun(x: number) {
+        return x;
+    }
+}
+
+var m: ^^typeof M = M;
+
+var adder = ^^new Adder(3, 4);
+^^delete (adder)
+
+var s = typeof {}
+interface I {}
+var j: I = {}
