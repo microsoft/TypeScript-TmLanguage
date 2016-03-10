26464684d0a83979fba4a5178980f9feef3b8fef
Anubha Mathur <anubmat@microsoft.com>
2016-03-10 10:14:48 -0800

    Added a testcase and eliminated space characters entirely

--- tests/cases/Issue89.ts
+++ tests/cases/Issue89.ts
@@ -13,3 +13,11 @@ export class ^^E ^^extends /**/^^H ^^ implements/*^^*/ ^^One, ^^Two, ^^Three{
 	^^	
 }
 ^^
+
+^^class ^^className ^^extends ^^class1 ^^implements ^^class2 {
+    ^^
+}
+
+/*
+	Checking comments ^^
+*/
