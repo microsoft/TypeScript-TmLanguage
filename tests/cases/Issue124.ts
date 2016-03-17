9360c2595f58c9ac2a4326dd6f47b1d977ab3f75
Anubha Mathur <anubmat@microsoft.com>
2016-03-17 10:49:23 -0700

    Adding capability for ternary operator

---
+++ tests/cases/Issue124.ts
@@ -0,0 +1,16 @@
+/*
+	Solving Issue #124, #80, #81
+	Adding capability of ternary strings.
+*/
+
+let a = ^^true ? ^^true : ^^true;  
+let b = ^^true ? ^^true : ^^`^^this is ^^${} highlighted`
+let c = ^^true ? ^^`^^hello` : ^^`^^this ${DEPENDENCY_SEPARATOR}${moduleName} is highlighted`;
+
+var ^^newVar = ^^a ^^? ^^(b == `hello`) : ^^true;
+
+var ^^debArch = ^^arch ^^=== ^^'x64' ^^? ^^'amd64' ^^: ^^'i386';
+
+/*
+	^^Comment
+*/
