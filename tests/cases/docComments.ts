aea13ef0e70dc12111f9e2ca071c74dc1e41c3fe
Sheetal Nandi <shkamat@microsoft.com>
2016-08-11 11:57:16 -0700

    Update comment grammar

---
+++ tests/cases/docComments.ts
@@ -0,0 +1,8 @@
+/**
+ * This function takes two parameters
+ * @param {string} param1 Some string param.
+ * @param {number} param2  Some number param.
+ * some other contents
+ */
+function foo(param1: string, param2: number) {
+}
