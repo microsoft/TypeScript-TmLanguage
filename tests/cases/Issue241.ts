543cc6a967a7957dbca1aab299b19f0f804a118d
Sheetal Nandi <shkamat@microsoft.com>
2016-08-17 15:56:00 -0700

    Fix the return type of function type when it is tuple or object type

---
+++ tests/cases/Issue241.ts
@@ -0,0 +1,15 @@
+function f(): () => number {
+    const x = 0;
+}
+function foo(): [number, () => number] {
+    const x = 0;
+}
+function f(): () => number {
+    const x = 0;
+}
+function f2(): () => {x: number }{
+    const x = 0;
+}
+function foo2(): [number, () => { x: number }] {
+    const x = 0;
+}
