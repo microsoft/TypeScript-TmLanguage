153f123525f2ee0e0a966760865afa535b985e7e
Sheetal Nandi <shkamat@microsoft.com>
2016-08-17 17:51:40 -0700

    Fix the fn return type annotation when it is used inside the object type

---
+++ tests/cases/Issue77.ts
@@ -0,0 +1,13 @@
+// trailing part of some real code...
+
+null, <{[key]: () => any}> null
+
+// no syntax highlighting from here down...
+
+function foo():{a: string} { }
+
+null, <{[key]: () =>{a: number}}> null
+
+// no syntax highlighting from here down...
+
+function foo() { }
