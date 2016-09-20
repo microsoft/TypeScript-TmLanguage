c8771885be05ea33afa8ac10b6a4868288b49590
Sheetal Nandi <shkamat@microsoft.com>
2016-09-20 15:00:43 -0700

    Generate TypeScriptReact theme

--- build/build.ts
+++ build/build.ts
@@ -8,25 +8,29 @@ function writePlistFile(grammar: any, fileName: string) {
     fs.writeFileSync(fileName, text, "utf8");
 }
 
-function readYamlGrammar(fileName: string) {
+function readYaml(fileName: string) {
     const text = fs.readFileSync(fileName, "utf8");
     return yaml.safeLoad(text);
 }
 
-function fixScopeNames(rule: any) {
+function changeTsToTsx(str: string) {
+    return str.replace(/\.ts/g, '.tsx');
+}
+
+function fixGrammarScopeNames(rule: any) {
     if (typeof rule.name === 'string') {
-        rule.name = rule.name.replace(/\.ts/g, '.tsx');
+        rule.name = changeTsToTsx(rule.name);
     }
     for (var property in rule) {
         var value = rule[property];
         if (typeof value === 'object') {
-            fixScopeNames(value);
+            fixGrammarScopeNames(value);
         }
     }
 }
 
-function changeToTsxGrammar(grammar: any) {
-    const tsxUpdates = readYamlGrammar("../TypeScriptReact.YAML-tmLanguage");
+function changeTsToTsxGrammar(grammar: any) {
+    const tsxUpdates = readYaml("../TypeScriptReact.YAML-tmLanguage");
 
     // Update name, file types, scope name and uuid
     for (let key in tsxUpdates) {
@@ -37,12 +41,12 @@ function changeToTsxGrammar(grammar: any) {
 
     // Update scope names to .tsx
     const repository = grammar.repository;
-    const updatesRepository = tsxUpdates.repository;
     for (let key in repository) {
-        fixScopeNames(repository[key]);
+        fixGrammarScopeNames(repository[key]);
     }
 
     // Add repository items
+    const updatesRepository = tsxUpdates.repository;
     for (let key in updatesRepository) {
         switch(key) {
             case "statements":
@@ -60,21 +64,47 @@ function changeToTsxGrammar(grammar: any) {
 }
 
 function buildGrammar() {
-    const tsGrammar = readYamlGrammar("../TypeScript.YAML-tmLanguage");
+    const tsGrammar = readYaml("../TypeScript.YAML-tmLanguage");
 
     // Write TypeScript.tmLanguage
     writePlistFile(tsGrammar, "../TypeScript.tmLanguage");
 
     // Write TypeScriptReact.tmLangauge
-    const tsxGrammar = changeToTsxGrammar(tsGrammar);
+    const tsxGrammar = changeTsToTsxGrammar(tsGrammar);
     writePlistFile(tsxGrammar, "../TypeScriptReact.tmLanguage");
 }
 
+function changeTsToTsxTheme(theme: any) {
+    const tsxUpdates = readYaml("../TypeScriptReact.YAML-tmTheme");
+
+    // Update name, uuid
+    for (let key in tsxUpdates) {
+        if (key !== "settings") {
+            theme[key] = tsxUpdates[key];
+        }
+    }
+
+    // Update scope names to .tsx
+    const settings = theme.settings;
+    for (let i = 0; i < settings.length; i++) {
+        settings[i].scope = changeTsToTsx(settings[i].scope);
+    }
+
+    // Add additional setting items
+    theme.settings = theme.settings.concat(tsxUpdates.settings);
+
+    return theme;
+}
+
 function buildTheme() {
-    const tsTheme = readYamlGrammar("../TypeScript.YAML-tmTheme");
+    const tsTheme = readYaml("../TypeScript.YAML-tmTheme");
 
     // Write TypeScript.tmTheme
     writePlistFile(tsTheme, "../TypeScript.tmTheme");
+
+    // Write TypeScriptReact.thTheme
+    const tsxTheme = changeTsToTsxTheme(tsTheme);
+    writePlistFile(tsxTheme, "../TypeScriptReact.tmTheme");
 }
 
 buildGrammar();
