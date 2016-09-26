25e792b647eaca64a84f565fb69092faa38bcd34
Sheetal Nandi <shkamat@microsoft.com>
2016-09-26 16:16:11 -0700

    Indexer is something that has identifier(parameter) followed by [ with or without type annotation Fixes #197

---
+++ tests/cases/Issue197.ts
@@ -0,0 +1,14 @@
+ export class sampleClass  {
+        public equipmentTypeList: string[] = ["FAX", "OFFICE PHONE"];
+        public pbxEquipmentType = "FAX";
+        public spliceRightStripName = "TE"; 
+        public cablePairStatusValues: any[] =  
+            [       
+                { statusText: "Assigned" },
+                { statusText: "Faulty" }, 
+                { statusText: "Not in Use" },
+            ];
+        public cablePairStatusValueForAssigned = "Assigned";
+        public cablePairStatusValueForFaulty = "Faulty";
+        public phoneCircuitType = "Phone";
+ }
