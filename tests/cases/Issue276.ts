52bb30d79bf89324421241cfaee6e52ba65d68dc
Sheetal Nandi <shkamat@microsoft.com>
2016-09-29 14:41:22 -0700

    Update jsdoc type and variable recognition Fixes #276

---
+++ tests/cases/Issue276.ts
@@ -0,0 +1,8 @@
+/**
+ * @param {string} value
+ * @param {string[]} arrayValue
+ * @param {string} [optionalValue]
+ */
+function test(value: string, arrayValue: string[], optionalValue?: string) {
+
+}
