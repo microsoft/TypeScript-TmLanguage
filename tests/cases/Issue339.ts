9a9895400d9d4ec10305a4aa9890a221293a5dfe
Sheetal Nandi <shkamat@microsoft.com>
2016-11-01 13:23:06 -0700

    Allow class expression as part of new expression target Fixes #339

---
+++ tests/cases/Issue339.ts
@@ -0,0 +1,22 @@
+function f() {
+
+    class implements BeingSpreading<Spreading> {
+        copy = copy;
+        fuse = fuse;
+        toInternalSpreading = toInternalSpreading;
+        toFiltersOnlySpreading = toFiltersOnlySpreading;
+        toResultsOnlySpreading = toResultsOnlySpreading;
+        toNormalSpreading = toNormalSpreading;
+        toNormalOrMaximizedSpreadingIfNotYet = ensureNormalOrMaximizedSpreading;
+    };
+
+    return new class implements BeingSpreading<Spreading> {
+        copy = copy;
+        fuse = fuse;
+        toInternalSpreading = toInternalSpreading;
+        toFiltersOnlySpreading = toFiltersOnlySpreading;
+        toResultsOnlySpreading = toResultsOnlySpreading;
+        toNormalSpreading = toNormalSpreading;
+        toNormalOrMaximizedSpreadingIfNotYet = ensureNormalOrMaximizedSpreading;
+    };
+}
