cea95d36a820753ba89d7a8205d8bfe9e3c6fed9
Sheetal Nandi <shkamat@microsoft.com>
2016-11-07 11:44:25 -0800

    Stop consuming spaces when determining function type and parenthesized type Fixes #347

---
+++ tests/cases/Issue347.ts
@@ -0,0 +1,12 @@
+const a = new Promise (resolve => {
+    const file = this.props.doc.file;
+    resolve({
+        abc: Math.random()
+    });
+
+    class Foo extends React.Component {
+        render() {
+        }
+    }
+    
+});
