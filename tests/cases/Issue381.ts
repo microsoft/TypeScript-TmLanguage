98ab3ba5cc0ae47c8080da657b7ea07a307eb5d3
Sheetal Nandi <shkamat@microsoft.com>
2016-12-12 11:46:42 -0800

    Handle the end of the new expression as look ahead of } as it can be possible in case of template expressions Fixes #381

---
+++ tests/cases/Issue381.ts
@@ -0,0 +1,11 @@
+class Foo{
+  constructor(){
+
+  }
+  test(){
+    return `/game.php?village=${source}&screen=main&ajaxaction=upgrade_building&type=main&h=${csrf_token}&&client_time=${+new Date}`
+  }
+  noColorSyntax(){
+    
+  }
+}
