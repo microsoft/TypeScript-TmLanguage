cd82eb66add83f97c6424c2afd30552b0555394b
Sheetal Nandi <shkamat@microsoft.com>
2016-12-14 12:42:12 -0800

    Fix arrow function determination logic to accomodate => present on next line Fixes #383

---
+++ tests/cases/Issue383.tsx
@@ -0,0 +1,13 @@
+// @onlyOwnGrammar - As this has jsx
+
+const singleLineArrow = (first: number, second: Set<string>) => (<div></div>);
+
+const multilineArrow = (first: number,
+	second: number[],
+) => (<div></div>);
+
+const multilineArrowWithGenericType = (first: number,
+	second: Set<string>,
+) => (<div></div>);
+
+const followup = (first: number, second: Set<string>) => (<div></div>);
