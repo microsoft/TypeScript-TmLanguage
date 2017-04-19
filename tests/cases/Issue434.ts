b68820e853b57344951aaad194d466008a029db2
Sheetal Nandi <shkamat@microsoft.com>
2017-04-19 14:39:27 -0700

    Treat only expression like <Identifier> on new line as cast expression Fixes #434

---
+++ tests/cases/Issue434.ts
@@ -0,0 +1,21 @@
+export class SomeClass {
+    private someMethod(): void {
+        if (1
+            < 2) {
+            console.log("TypeScript syntax highlighting is broken!");
+        }
+        else if (1 > 2) {
+            console.log("");
+        }
+    }
+
+    private someOtherMethod() {
+        console.log("");
+        //
+        const q = 1;
+        var s = 2;
+        let e = 3;
+    }
+
+    q = 100;
+}
