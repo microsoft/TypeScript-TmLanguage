0d2bd8f1ddef1e3016fb1a6a3bc01653338de24a
Sheetal Nandi <shkamat@microsoft.com>
2017-04-21 15:54:53 -0700

    Update jsdoc regex to atom/language-javascript#70a6c530b8fdc678086e2883e348fb909c9e4b77 Fixes #445 and #435

---
+++ tests/cases/Issue435.ts
@@ -0,0 +1,7 @@
+/**
+     * Manually add an item to the uploading queue.
+     *
+     * @param {PIXI.DisplayObject|PIXI.Container|PIXI.BaseTexture|PIXI.Texture|PIXI.Graphics|PIXI.Text|*} item - Object to
+     *        add to the queue
+     * @return {PIXI.CanvasPrepare} Instance of plugin for chaining.
+     */
---
+++ tests/cases/Issue445.ts
@@ -0,0 +1,8 @@
+/**
+ * My awesome function.
+ * @param {{id: string, name: string}} object An object with an id and name field.
+ */
+function myAwesomeFunction(object) {
+  const { name, id } = object.
+  // do stuff with name and id
+}
