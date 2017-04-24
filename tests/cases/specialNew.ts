9f6676aa2ddb75cb5a9dbe1f59024069e839d986
Sheetal Nandi <shkamat@microsoft.com>
2017-04-24 16:03:35 -0700

    Let the new expression end if its a function expression start

---
+++ tests/cases/specialNew.ts
@@ -0,0 +1,9 @@
+let storeSomeObj = new function () {
+ this.hasNoColorization = function () {
+ let butThisDoes = "yay";
+ }
+
+ let noColorHereEither = "I'm not colorized";
+ }
+
+ let butOutSide = "all color is back to normal!";
