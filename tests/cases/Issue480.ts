ce5f7c96a7f5fd98accb9fa255b103a4aa0e4a2e
Sheetal Nandi <shkamat@microsoft.com>
2017-06-28 12:02:31 -0700

    Recognize @internal in the line and block comments Fixes #480

---
+++ tests/cases/Issue480.ts
@@ -0,0 +1,11 @@
+/* @internal */
+export interface Pattern {
+        prefix: string;
+        suffix: string;
+}
+
+// @internal
+export interface Pattern2 {
+        prefix: string;
+        suffix: string;
+}
