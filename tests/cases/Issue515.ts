b00e2fa274357428267f86814f27c762082b8b35
Matt Bierner <matb@microsoft.com>
2017-09-11 17:48:30 -0700

    Fix jsdoc hang on default parameter value Fixes #515

---
+++ tests/cases/Issue515.ts
@@ -0,0 +1,9 @@
+/**
+ * @param {boolean} [settings.debug= - Print getUpdate results in console.
+ */
+/**
+ * @param {boolean} [settings.debug=] - Print getUpdate results in console.
+ */
+/**
+ * @param {boolean} [settings.debug=1] - Print getUpdate results in console.
+ */
