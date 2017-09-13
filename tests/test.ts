a5f3894dd3ae3b313bc446f4a34db4520ba679ed
Matt Bierner <matb@microsoft.com>
2017-09-13 14:22:49 -0700

    Lazily run generateScopes in tests
    **Bug**
    In the tests, if the grammar causes a hang we do not handle this currently. This is because `generateScopes` is being run in the `describe` block instead of inside one of the tests
    
    **Fix**
    Move `generateScopes` into the `it` block of the test

--- tests/test.ts
+++ tests/test.ts
@@ -22,27 +22,27 @@ ensureCleanGeneratedFolder();
 // Generate the new baselines
 for (const fileName of fs.readdirSync('cases')) {
     describe("Generating baseline for " + fileName, () => {
-        const text = fs.readFileSync(path.join('./cases', fileName), 'utf8');
-        const parsedFileName = path.parse(fileName);
-        const { markerScopes, wholeBaseline } = build.generateScopes(text, parsedFileName);
-        if (markerScopes) {
-            addTestCase(parsedFileName.name + '.txt', markerScopes);
-        }
-        addTestCase(parsedFileName.name + '.baseline.txt', wholeBaseline);
+        it('Comparing generated', () => {
+            const text = fs.readFileSync(path.join('./cases', fileName), 'utf8');
+            const parsedFileName = path.parse(fileName);
+            const { markerScopes, wholeBaseline } = build.generateScopes(text, parsedFileName);
+            if (markerScopes) {
+                assertBaselinesMatch(parsedFileName.name + '.txt', markerScopes);
+            }
+            assertBaselinesMatch(parsedFileName.name + '.baseline.txt', wholeBaseline);
+        });
     });
 }
 
-function addTestCase(file: string, generatedText: string) {
+function assertBaselinesMatch(file: string, generatedText: string) {
     const generatedFileName = path.join(generatedFolder, file);
     fs.writeFileSync(generatedFileName, generatedText);
 
-    it('Comparing generated' + file, () => {
-        const baselineFile = path.join(baselineFolder, file);
-        if (fs.existsSync(baselineFile)) {
-            chai.assert.equal(generatedText, fs.readFileSync(baselineFile, 'utf8'), "Expected baselines to match");
-        }
-        else {
-            chai.assert(false, "New generated baseline");
-        }
-    });
+    const baselineFile = path.join(baselineFolder, file);
+    if (fs.existsSync(baselineFile)) {
+        chai.assert.equal(generatedText, fs.readFileSync(baselineFile, 'utf8'), "Expected baselines to match: " + file);
+    }
+    else {
+        chai.assert(false, "New generated baseline");
+    }
 }
