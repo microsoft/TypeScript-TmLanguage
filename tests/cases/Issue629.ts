9a52efafc40313af7deb0bea827294b2b215dda4
Sheetal Nandi <shkamat@microsoft.com>
2018-06-14 12:55:37 -0700

    Handle the parameter name scoping in function type Fixes #629

---
+++ tests/cases/Issue629.ts
@@ -0,0 +1,18 @@
+class c {
+	private _forEachCell(
+    terminal: ITerminal,
+    firstRow: number,
+    lastRow: number,
+    callback: (
+      code: number,
+      char: string,
+      width: number,
+      x: number,
+      y: number,
+      fg: number,
+      bg: number,
+      flags: number
+    ) => void
+  ): void {
+	}
+}
