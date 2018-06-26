b35f9e97d0b5a0995dd7addc1aac90760e32bff2
Ron Buckton <ron.buckton@microsoft.com>
2018-06-25 17:45:23 -0700

    Add tests

--- tests/cases/directives.ts
+++ tests/cases/directives.ts
@@ -1,4 +1,5 @@
 ///<reference path="somePath.ts"/>
+///<reference lib="es2015"/>
 ///<amd-dependency path="somePath.ts"/>
 ///<amd-module name="modulename"/>
 ///<amd-dependency path="someOtherPath.ts" name="someOtherModule"/>
