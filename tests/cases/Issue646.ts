14e9cf1a754552fb8d2c6aa24a3fd4839ef53851
Sheetal Nandi <shkamat@microsoft.com>
2018-08-21 10:48:24 -0700

    Do not consume end of line in a single line comment Fixes #646

---
+++ tests/cases/Issue646.ts
@@ -0,0 +1,11 @@
+// Good
+const foo = 5; // The json data that the extension sent
+console.log(`${foo}`)
+
+// Good
+const foo = 5
+console.log(`${foo}`)
+
+// Bad
+const foo = 5 // The json data that the extension sent
+console.log(`${foo}`)
