b80870c2c1eac64abb155db17220baee85d85689
Sheetal Nandi <shkamat@microsoft.com>
2019-01-22 09:42:35 -0800

    Add const assertion as per Microsoft/TypeScript#29510

---
+++ tests/cases/constType.ts
@@ -0,0 +1,4 @@
+let r2 = { x: 20, y: 10 } as const;
+let r2 = {...p} as const;
+let p1 = { x: 10, y: 20 as const };
+let p2 = { 10 as const, 'foo' as const };
---
+++ tests/cases/constTypeAssert.ts
@@ -0,0 +1,2 @@
+// @onlyOwnGrammar - As this has type assertion
+let r1 = <const>{ x: 10, y: 29, foo() { this.x = 20 } };
