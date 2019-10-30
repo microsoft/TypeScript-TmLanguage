477c1b17e273b64af13040c064c9ed62c8b32fba
Sheetal Nandi <shkamat@microsoft.com>
2019-10-30 14:27:13 -0700

    Fix detection of function call when called after another function call Fixes #785

---
+++ tests/cases/Issue785.ts
@@ -0,0 +1,17 @@
+function getFoo() { return <T, P>(t: T) => {t}}
+
+const stuff = { foo: getFoo()<string, number>('ddd') }
+
+//const v = {
+//  title: ''
+//}
+
+console.log('highlight is broken unless object literal defined above')
+const a = ''
+function hello() {
+  return 10
+}
+
+interface Bar {
+  count: number
+}
