dc5fe357ee5582244b798917a5fc9d7cd73e6220
Sheetal Nandi <shkamat@microsoft.com>
2019-11-26 11:37:03 -0800

    Handle possibly parameters when there is no type annotation Fixes #791

---
+++ tests/cases/Issue791.ts
@@ -0,0 +1,16 @@
+const test = (foo, bar) => `${foo} ${bar}`
+
+const test2 = (
+  foo,
+  bar
+) => `${foo} ${bar}`
+
+function test3(foo, bar) {
+  return `${foo} ${bar}`
+}
+function test4(
+  foo,
+  bar
+) {
+  return `${foo} ${bar}`
+}
