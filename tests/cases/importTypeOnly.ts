163205147a0cd70ef2f912f86d7fab53a4f9e501
Andrew Branch <andrewbranch@users.noreply.github.com>
2020-01-07 10:32:48 -0600

    Type-only imports and exports (#792)
    * Support type-only imports and exports
    
    * Fix copy/paste error
    
    * Update baseline
    
    * Handle import export type at local matching rules
    
    Co-authored-by: Sheetal Nandi <shkamat@microsoft.com>

---
+++ tests/cases/importTypeOnly.ts
@@ -0,0 +1,6 @@
+import type T from "mod";
+import type { U, V } from "mod";
+import type * as types from "mod";
+import type from "mod";
+import type = require("mod");
+import type T = require("mod");
