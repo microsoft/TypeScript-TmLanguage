4d30ff834ec324f56291addd197aa1e423cedfdd
Ron Buckton <ron.buckton@microsoft.com>
2022-03-18 16:43:35 -0700

    Updated to support 'infer..extends'

--- tests/cases/inferTypes.ts
+++ tests/cases/inferTypes.ts
@@ -2,3 +2,4 @@ type ReturnType<T extends Function> = T extends ((...args: any[]) => infer R) | 
 type ArgumentType<T extends (x: any) => any> = T extends (a: infer A) => any ? A : any;
 type X1<T extends { x: any, y: any }> = T extends { x: infer X, y: infer Y } ? [X, Y] : any;
 type B1<S> = S extends A1<infer T, infer U> ? [T, U] : never;
+type C1<S> = S extends A1<infer T extends number, infer U extends string> ? [T, U] : never;
