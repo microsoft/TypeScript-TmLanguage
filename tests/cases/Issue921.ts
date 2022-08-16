cc258c348cbd827be5ddc8b8b41c44aac0a2f968
Sheetal Nandi <shkamat@microsoft.com>
2022-08-16 16:54:46 -0700

    Fix incorrect instance of end Fixes #921

---
+++ tests/cases/Issue921.ts
@@ -0,0 +1,4 @@
+children0 instanceof Child === true;
+children0 instanceof Child.prototype.constructor === true;
+children0 instanceof(Child.prototype.constructor) === true;
+children0 instanceof dict.CONST.ctor === true;
