abfece0c496246f3110decfe747a6b86b346d65a
Sheetal Nandi <shkamat@microsoft.com>
2023-02-08 15:56:09 -0800

    Handle end of comment in the variable initializer Fixes #948

---
+++ tests/cases/issue948.ts
@@ -0,0 +1,13 @@
+class A {
+  #a =
+      /** @param {MouseEvent} arg */
+      arg => {
+          const someV = 132
+          if (true) {
+              doSomething()
+          }
+          while(false) {
+              
+          }
+      }
+}
